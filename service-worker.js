c9d66594e25b2cf59a6ea1dba48dc33ce166d393
Pardhu Madipalli <pardhu1212@gmail.com>
2021-02-27 00:51:09 +0530

    Write new html page for AWDO (#12)

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@ var preLoad = function(){
     console.log("Installing web app");
     return caches.open("offline").then(function(cache) {
         console.log("caching index and important routes");
-        return cache.addAll(["/", "/software", "/education","/image-segmentation-using-awdo","image-segmentation-using-wdo" ,"/offline.html"]);
+        return cache.addAll(["/", "/software", "/education","/image-segmentation-awdo","image-segmentation-wdo" ,"/offline.html"]);
     });
 };
 
