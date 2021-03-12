37fe938782c87ee5bc8d210591d3e3ae8ca7a563
Pardhu Madipalli <pardhu1212@gmail.com>
2021-03-13 04:53:30 +0530

    Create sidebar for mobile site

--- assets/js/custom-fun.js
+++ assets/js/custom-fun.js
@@ -2,9 +2,25 @@ const tableSkills = document.querySelectorAll('td:not(.cat)');
 const highlightClassList = ['cloud', 'server', 'ui', 'scripting', 'testing', 'bigdata', 'cicd']
 const highlightClassSelector = '.' + highlightClassList.join(',.')
 const highLightElems = document.querySelectorAll(highlightClassSelector);
+const sidenav = document.querySelector('#sidenav-open');
+
 highLightElems.forEach(el => el.addEventListener('mouseover', event => highlight(event)));
 highLightElems.forEach(el => el.addEventListener('mouseout', event => remhighlight(event)));
 
+const closenav = document.querySelector('#sidenav-close');
+const opennav = document.querySelector('#sidenav-button');
+
+sidenav.addEventListener('transitionend', e => {
+    if (e.propertyName !== 'transform') {
+        return;
+    }
+
+    const isOpen = document.location.hash === '#sidenav-open';
+
+    isOpen
+        ? closenav.focus()
+        : opennav.focus();
+});
 
 function highlight(event) {
     let targetClassList = event.target.classList
