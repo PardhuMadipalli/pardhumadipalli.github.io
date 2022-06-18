2969c6ff5a7493cdad46a10d133f48b1930dfd87
Pardhu Madipalli <pardhu1212@gmail.com>
2022-06-18 17:55:52 +0530

    Updated contents of various pages

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@ var preLoad = function(){
     console.log("Installing web app");
     return caches.open("offline").then(function(cache) {
         console.log("caching index and important routes");
-        return cache.addAll(["/", "/software", "/education","/image-segmentation-awdo","image-segmentation-wdo" ,"/offline.html"]);
+        return cache.addAll(["/", "/work", "/education","/image-segmentation-awdo","image-segmentation-wdo" ,"/offline.html"]);
     });
 };
 
